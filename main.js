022830251af0aad5fcde93723cf367269040cd41
Jesus Rincon <jeseril2213@gmail.com>
2023-05-12 22:02:10 -0500

    se anade interaccion en todas las pag

--- main.js
+++ main.js
@@ -2,9 +2,24 @@ const cerrarMenu = document.querySelector('.cerrar-menu');
 const abrirMenu = document.querySelector('.menu-hamburguesa');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const btHome = document.querySelector('.boton-home');
+const btHabilidades = document.querySelector('.boton-habilidades');
+const btPortfolio = document.querySelector('.boton-portfolio');
+const btContacto = document.querySelector('.boton-contacto');
+
+const Home = document.querySelector('.home');
+const Habilidades = document.querySelector('.habilidades');
+const Portfolio = document.querySelector('.portfolio');
+const Contacto = document.querySelector('.contacto');
+
 
 cerrarMenu.addEventListener('click',cerrarMenuFunction);
 abrirMenu.addEventListener('click',abrirMenuFunction);
+
+btHome.addEventListener('click',abrirSeccionHome)
+btHabilidades.addEventListener('click',abrirSeccionHabilidades)
+btPortfolio.addEventListener('click',abrirSeccionPortfolio)
+btContacto.addEventListener('click',abrirSeccionContacto)
 
 function cerrarMenuFunction () {
     mobileMenu.id = 'oculto';
@@ -13,4 +28,32 @@ function cerrarMenuFunction () {
 function abrirMenuFunction () {
     mobileMenu.removeAttribute("id");
     console.log("hola")
+}
+
+function abrirSeccionHome(){
+    Home.removeAttribute("id");
+    Habilidades.id ='oculto';
+    Portfolio.id ='oculto';
+    Contacto.id ='oculto';
+}
+
+function abrirSeccionHabilidades(){
+    Habilidades.removeAttribute("id");
+    Home.id ='oculto';
+    Portfolio.id ='oculto';
+    Contacto.id ='oculto';
+}
+
+function abrirSeccionPortfolio(){
+    Portfolio.removeAttribute("id");
+    Home.id ='oculto';
+    Habilidades.id ='oculto';
+    Contacto.id ='oculto';
+}
+
+function abrirSeccionContacto(){
+    Contacto.removeAttribute("id");
+    Home.id ='oculto';
+    Habilidades.id ='oculto';
+    Portfolio.id ='oculto';
 }
