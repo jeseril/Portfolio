898a6b0217815f09e6342bf40ce200d988b3d04d
Jesus Rincon <jeseril2213@gmail.com>
2023-05-12 23:20:34 -0500

    se corrige la seccion portfolio

--- main.js
+++ main.js
@@ -2,24 +2,64 @@ const cerrarMenu = document.querySelector('.cerrar-menu');
 const abrirMenu = document.querySelector('.menu-hamburguesa');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-const btHome = document.querySelector('.boton-home');
-const btHabilidades = document.querySelector('.boton-habilidades');
-const btPortfolio = document.querySelector('.boton-portfolio');
-const btContacto = document.querySelector('.boton-contacto');
+const btHome = document.querySelectorAll('.boton-home');
+const btHabilidades = document.querySelectorAll('.boton-habilidades');
+const btPortfolio = document.querySelectorAll('.boton-portfolio');
+const btContacto = document.querySelectorAll('.boton-contacto');
 
 const Home = document.querySelector('.home');
 const Habilidades = document.querySelector('.habilidades');
 const Portfolio = document.querySelector('.portfolio');
 const Contacto = document.querySelector('.contacto');
 
-
 cerrarMenu.addEventListener('click',cerrarMenuFunction);
 abrirMenu.addEventListener('click',abrirMenuFunction);
 
-btHome.addEventListener('click',abrirSeccionHome)
-btHabilidades.addEventListener('click',abrirSeccionHabilidades)
-btPortfolio.addEventListener('click',abrirSeccionPortfolio)
-btContacto.addEventListener('click',abrirSeccionContacto)
+
+btHome.forEach(boton => {
+    boton.addEventListener('click',(event) => {
+        event.preventDefault();
+        Home.removeAttribute("id");
+        Habilidades.id ='oculto';
+        Portfolio.id ='oculto';
+        Contacto.id ='oculto';
+        mobileMenu.id = 'oculto';
+    });
+  });
+
+  btHabilidades.forEach(boton => {
+    boton.addEventListener('click',(event) => {
+        event.preventDefault();
+        Habilidades.removeAttribute("id");
+        Home.id ='oculto';
+        Portfolio.id ='oculto';
+        Contacto.id ='oculto';
+        mobileMenu.id = 'oculto';
+    });
+  });
+
+  btPortfolio.forEach(boton => {
+    boton.addEventListener('click',(event) => {
+        event.preventDefault();
+        Portfolio.removeAttribute("id");
+        Home.id ='oculto';
+        Habilidades.id ='oculto';
+        Contacto.id ='oculto';
+        mobileMenu.id = 'oculto';
+    });
+  });
+
+  btContacto.forEach(boton => {
+    boton.addEventListener('click',(event) => {
+        event.preventDefault();
+        Contacto.removeAttribute("id");
+        Home.id ='oculto';
+        Habilidades.id ='oculto';
+        Portfolio.id ='oculto';
+        mobileMenu.id = 'oculto';
+    });
+  });
+
 
 function cerrarMenuFunction () {
     mobileMenu.id = 'oculto';
@@ -28,32 +68,4 @@ function cerrarMenuFunction () {
 function abrirMenuFunction () {
     mobileMenu.removeAttribute("id");
     console.log("hola")
-}
-
-function abrirSeccionHome(){
-    Home.removeAttribute("id");
-    Habilidades.id ='oculto';
-    Portfolio.id ='oculto';
-    Contacto.id ='oculto';
-}
-
-function abrirSeccionHabilidades(){
-    Habilidades.removeAttribute("id");
-    Home.id ='oculto';
-    Portfolio.id ='oculto';
-    Contacto.id ='oculto';
-}
-
-function abrirSeccionPortfolio(){
-    Portfolio.removeAttribute("id");
-    Home.id ='oculto';
-    Habilidades.id ='oculto';
-    Contacto.id ='oculto';
-}
-
-function abrirSeccionContacto(){
-    Contacto.removeAttribute("id");
-    Home.id ='oculto';
-    Habilidades.id ='oculto';
-    Portfolio.id ='oculto';
 }
